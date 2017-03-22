847b49631119b017639ebf2d7fe4f4774e5361ae
BhumikaMarsoniya <bhumika.marsoniya@gmail.com>
2017-03-22 22:57:02 +0530

    Add coursera assignment 4

---
+++ module4-solution/src/categories.controller.js
@@ -0,0 +1,13 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.controller('CategoriesController', CategoriesController);
+// 'item' is injected through state's resolve
+CategoriesController.$inject = ['categories']
+function CategoriesController(categories) {
+  var categoriesCtrl = this;
+  categoriesCtrl.categories = categories;
+}
+
+})();
